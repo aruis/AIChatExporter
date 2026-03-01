bfaf56880436b712f581bd9afbe2f453e84fdbcc
Liu Rui <lovearuis@gmail.com>
2026-03-01 21:57:26 +0800

    Initial Commit

---
+++ exportAIChat/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  exportAIChat
+//
+//  Created by 牧云踏歌 on 2026/3/1.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ exportAIChatTests/exportAIChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  exportAIChatTests.swift
+//  exportAIChatTests
+//
+//  Created by 牧云踏歌 on 2026/3/1.
+//
+
+import Testing
+@testable import exportAIChat
+
+struct exportAIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
